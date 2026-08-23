6c313a32543208c5d4f79c57f0e0fad6343c4ad5
hanlinh227-ship-it <hanlinh227@gmail.com>
2026-08-23 12:31:09 +0700

    feat: recalibrate V11 for practical scalp signal frequency

--- cloudflare-worker/v11/config.js
+++ cloudflare-worker/v11/config.js
@@ -1,2 +1,14 @@
-export const V11_CONFIG=Object.freeze({version:'V11.0.0-foundation',td:{creditsPerMinute:55,reserveLifecycle:5,reserveVerification:4,discoveryBudget:46},quoteTtlSec:{crypto:8,forex:25,metal:20,index:20},scanCadenceSec:{crypto:60,forex:120,metal:120,index:180},markets:{crypto:{quality:62,minRR:1.08,horizonMin:90,deepMax:8},forex:{quality:68,minRR:1.20,horizonMin:180,deepMax:6},metal:{quality:70,minRR:1.25,horizonMin:180,deepMax:4},index:{quality:68,minRR:1.20,horizonMin:180,deepMax:5}}});
+export const V11_CONFIG=Object.freeze({
+ version:'V11.1-scalp',
+ mode:'SCALP',
+ td:{creditsPerMinute:55,reserveLifecycle:5,reserveVerification:4,discoveryBudget:46},
+ quoteTtlSec:{crypto:12,forex:35,metal:30,index:30},
+ scanCadenceSec:{crypto:45,forex:75,metal:75,index:90},
+ markets:{
+  crypto:{quality:55,minRR:1.00,horizonMin:45,deepMax:10,scalpTargetAtr:.70},
+  forex:{quality:58,minRR:1.05,horizonMin:60,deepMax:8,scalpTargetAtr:.65},
+  metal:{quality:60,minRR:1.08,horizonMin:60,deepMax:6,scalpTargetAtr:.70},
+  index:{quality:58,minRR:1.05,horizonMin:60,deepMax:7,scalpTargetAtr:.65}
+ }
+});
 export const V11_GROUPS=Object.freeze(['crypto','forex','metal','index']);
