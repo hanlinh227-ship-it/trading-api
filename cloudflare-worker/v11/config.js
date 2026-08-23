7fbf0351c6766d475042bec30407580d869e92df
hanlinh227-ship-it <hanlinh227@gmail.com>
2026-08-23 14:11:19 +0700

    chore: mark V11.4 V77/V78 entry restore

--- cloudflare-worker/v11/config.js
+++ cloudflare-worker/v11/config.js
@@ -1,14 +1,14 @@
 export const V11_CONFIG=Object.freeze({
- version:'V11.3-micro-scalp',
+ version:'V11.4-v77v78-entry',
  mode:'SCALP',
  td:{creditsPerMinute:55,reserveLifecycle:5,reserveVerification:4,discoveryBudget:46},
  quoteTtlSec:{crypto:12,forex:35,metal:30,index:30},
  scanCadenceSec:{crypto:45,forex:75,metal:75,index:90},
  markets:{
-  crypto:{quality:55,minRR:1.00,horizonMin:45,deepMax:10,scalpTargetAtr:.55},
-  forex:{quality:58,minRR:1.02,horizonMin:45,deepMax:8,scalpTargetAtr:.45},
-  metal:{quality:60,minRR:1.03,horizonMin:45,deepMax:6,scalpTargetAtr:.50},
-  index:{quality:58,minRR:1.02,horizonMin:45,deepMax:7,scalpTargetAtr:.46}
+  crypto:{quality:53,minRR:1.00,horizonMin:60,deepMax:10,scalpTargetAtr:1.00},
+  forex:{quality:55,minRR:1.00,horizonMin:60,deepMax:8,scalpTargetAtr:.95},
+  metal:{quality:56,minRR:1.03,horizonMin:60,deepMax:6,scalpTargetAtr:1.05},
+  index:{quality:54,minRR:1.02,horizonMin:60,deepMax:7,scalpTargetAtr:1.00}
  }
 });
 export const V11_GROUPS=Object.freeze(['crypto','forex','metal','index']);
