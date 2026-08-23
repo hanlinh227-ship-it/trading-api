a5a61ca8daba322ac9a364425a8e7c9cd3ef2a2a
hanlinh227-ship-it <hanlinh227@gmail.com>
2026-08-23 14:01:45 +0700

    chore: mark V11.3 micro-scalp exit model

--- cloudflare-worker/v11/config.js
+++ cloudflare-worker/v11/config.js
@@ -1,14 +1,14 @@
 export const V11_CONFIG=Object.freeze({
- version:'V11.2-symbol-scalp',
+ version:'V11.3-micro-scalp',
  mode:'SCALP',
  td:{creditsPerMinute:55,reserveLifecycle:5,reserveVerification:4,discoveryBudget:46},
  quoteTtlSec:{crypto:12,forex:35,metal:30,index:30},
  scanCadenceSec:{crypto:45,forex:75,metal:75,index:90},
  markets:{
-  crypto:{quality:55,minRR:1.00,horizonMin:45,deepMax:10,scalpTargetAtr:.70},
-  forex:{quality:58,minRR:1.05,horizonMin:60,deepMax:8,scalpTargetAtr:.65},
-  metal:{quality:60,minRR:1.08,horizonMin:60,deepMax:6,scalpTargetAtr:.70},
-  index:{quality:58,minRR:1.05,horizonMin:60,deepMax:7,scalpTargetAtr:.65}
+  crypto:{quality:55,minRR:1.00,horizonMin:45,deepMax:10,scalpTargetAtr:.55},
+  forex:{quality:58,minRR:1.02,horizonMin:45,deepMax:8,scalpTargetAtr:.45},
+  metal:{quality:60,minRR:1.03,horizonMin:45,deepMax:6,scalpTargetAtr:.50},
+  index:{quality:58,minRR:1.02,horizonMin:45,deepMax:7,scalpTargetAtr:.46}
  }
 });
 export const V11_GROUPS=Object.freeze(['crypto','forex','metal','index']);
