3a87f9d8358b6aac777960a114e90ba8db2909dc
hanlinh227-ship-it <hanlinh227@gmail.com>
2026-09-04 10:50:04 +0700

    Remove retired V10 unified entry hub

--- cloudflare-worker/hub-v10-unified-entry.js
+++
@@ -1,7 +0,0 @@
-import unifiedUx from "./hub-v10-unified-v2.js";
-import {scheduledV10} from "./signal-v10-scheduled-v2.js";
-
-export default {
-  async fetch(req,env,ctx){return unifiedUx.fetch(req,env,ctx);},
-  async scheduled(event,env,ctx){return scheduledV10(event,env,ctx);}
-};
