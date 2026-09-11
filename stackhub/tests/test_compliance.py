2132926576fb720572bd8f903c9c2c7826792bba
hanlinh227-ship-it <hanlinh227@gmail.com>
2026-09-11 18:57:33 +0700

    test: add STACKHUB compliance red test

---
+++ stackhub/tests/test_compliance.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class ComplianceTests(unittest.TestCase):
+    def test_placeholder(self):
+        self.fail("RED: STACKHUB compliance not implemented yet")
+
+
+if __name__ == "__main__":
+    unittest.main()
